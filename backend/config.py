0398f50082211aedb0282bf40dd24b3c8cb1b086
kristofermc <114157778+k-j-mc@users.noreply.github.com>
2023-02-22 23:32:32 +0200

    Commit 1.1

--- backend/config.py
+++ backend/config.py
@@ -4,8 +4,8 @@ import os
 BASE_DIR=os.path.dirname(os.path.realpath(__file__))
 
 class Config:
-    SECRET_KEY=config('SECRET_KEY')
-    SQLALCHEMY_TRACK_MODIFICATIONS=config('SQLALCHEMY_TRACK_MODIFICATIONS', cast=bool)
+    # SECRET_KEY=config('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS=config('SQLALCHEMY_TRACK_MODIFICATIONS', False)
 
 class DevConfig(Config):
     SQLALCHEMY_DATABASE_URI='sqlite:///dev.db'
